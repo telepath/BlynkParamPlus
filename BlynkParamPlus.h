699dc4f12b326cd4ae395392a1e8be121d694440
Telepath <cad@dev.telepath.de>
2018-10-11 22:02:05 +0200

    add operators for int, float, char*

--- BlynkParamPlus.h
+++ BlynkParamPlus.h
@@ -11,8 +11,19 @@ public:
       : BlynkParam(malloc(sizeof(BLYNK_PARAM_PLACEHOLDER_64)), 0, sizeof(BLYNK_PARAM_PLACEHOLDER_64))
   {}
     BlynkParamPlus operator=(const BlynkParam param);
+    BlynkParamPlus operator=(int param);
+    BlynkParamPlus operator=(float param);
+    BlynkParamPlus operator=(char* param);
     BlynkParamPlus operator+(const BlynkParam param);
+
+private:
+    void reset();
 };
+
+inline void BlynkParamPlus::reset(){
+  len=0;
+  *buff = '\0';
+}
 
 inline
 BlynkParamPlus BlynkParamPlus::operator+(const BlynkParam param)
@@ -37,10 +48,32 @@ BlynkParamPlus BlynkParamPlus::operator+(const BlynkParam param)
 inline
 BlynkParamPlus BlynkParamPlus::operator=(const BlynkParam param)
 {
-  // Serial.println("Reset length");
-  len=0;
-  *buff = '\0';
+  this->reset();
   return *this + param;
+}
+
+inline
+BlynkParamPlus BlynkParamPlus::operator=(int param)
+{
+  this->reset();
+  this->add(param);
+  return *this;
+}
+
+inline
+BlynkParamPlus BlynkParamPlus::operator=(float param)
+{
+  this->reset();
+  this->add(param);
+  return *this;
+}
+
+inline
+BlynkParamPlus BlynkParamPlus::operator=(char* param)
+{
+  this->reset();
+  this->add(param);
+  return *this;
 }
 
 #endif
